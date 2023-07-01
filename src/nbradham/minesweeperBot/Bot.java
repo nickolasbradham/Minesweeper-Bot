ba09d1af7c252eea9c9ba9fefaaef275305abe98
nickolasbradham <nickolasbradham@gmail.com>
2023-07-01 11:06:24 -0600

    Thinking about dropping queues and just iterating over field.

--- src/nbradham/minesweeperBot/Bot.java
+++ src/nbradham/minesweeperBot/Bot.java
@@ -1,6 +1,9 @@
 package nbradham.minesweeperBot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * The agen that tries to solve the Minesweeper game.
@@ -10,7 +13,13 @@ import java.util.Arrays;
  */
 final class Bot {
 
+	private static enum Phase {
+		FLAG_REMAINING, REVEAL_IF_SAFE
+	};
+
+	private final Queue<int[]> queue = new LinkedList<>();
 	private final Game game;
+	private final Phase[][] phase;
 
 	/**
 	 * Constructs a new Bot for {@code inputGame}.
@@ -19,16 +28,90 @@ final class Bot {
 	 */
 	private Bot(Game inputGame) {
 		game = inputGame;
+		phase = new Phase[game.getWidth()][game.getHeight()];
+		for (byte x = 0; x < phase.length; ++x)
+			Arrays.fill(phase[x], Phase.FLAG_REMAINING);
 	}
 
 	/**
 	 * Attempts to solve the game.
 	 */
 	private void solve() {
-		System.out.println("Starting field:");
+		printField("Starting field:");
+		queue.addAll(Arrays.asList(reveal("Opening Reveal", 1, 1)));
+		ArrayList<int[]> tmp = new ArrayList<>();
+		while (queue.size() != 0) {
+			int[] cords = queue.poll();
+			System.out.printf("Processing %s...%n", Arrays.toString(cords));
+			byte[] flags = { 0 };
+			switch (phase[cords[0]][cords[1]]) {
+			case FLAG_REMAINING:
+				tmp.clear();
+				game.forNeighbors(cords[0], cords[1], (x, y) -> {
+					if (game.isHidden(x, y))
+						tmp.add(new int[] { x, y });
+				});
+				if (tmp.size() == game.getMineCount(cords[0], cords[1])) {
+					System.out.println("Flagging remaining tiles...");
+					tmp.forEach(at -> {
+						game.flag(at[0], at[1]);
+						game.forNeighbors(at[0], at[1], (x, y) -> {
+							queueUp(x, y, Phase.REVEAL_IF_SAFE);
+						});
+					});
+					break;
+				} else
+					phase[cords[0]][cords[1]] = Phase.REVEAL_IF_SAFE;
+			case REVEAL_IF_SAFE:
+				flags[0] = 0;
+				tmp.clear();
+				game.forNeighbors(cords[0], cords[1], (x, y) -> {
+					if (game.isFlagged(x, y))
+						++flags[0];
+					else if (game.isHidden(x, y))
+						tmp.add(new int[] { x, y });
+				});
+				if (flags[0] == game.getMineCount(cords[0], cords[1]))
+					tmp.forEach(xy -> {
+						queueUp(reveal("Safe Reveal", xy[0], xy[1]));
+						game.forNeighbors(xy[0], xy[1], (x, y) -> queueUp(x, y, Phase.FLAG_REMAINING));
+					});
+				break;
+			default:
+				System.out.printf("Skipped %s.%n", Arrays.toString(cords));
+			}
+			printField("After Iteration:");
+		}
+	}
+
+	private void queueUp(int[][] arr) {
+		for (int[] xy : arr)
+			queueUp(xy[0], xy[1], Phase.FLAG_REMAINING);
+	}
+
+	private void queueUp(int x, int y, Phase chkPhase) {
+		if (game.isRevealed(x, y) && notQueued(x, y)) {
+			queue.offer(new int[] { x, y });
+			phase[x][y] = chkPhase;
+		}
+	}
+
+	private boolean notQueued(int x, int y) {
+		for (int[] cords : queue)
+			if (cords[0] == x && cords[1] == y)
+				return false;
+		return true;
+	}
+
+	private int[][] reveal(String label, int x, int y) {
+		int[][] revealed = game.reveal(x, y);
+		System.out.printf("%s (%d, %d): %s%n", label, x, y, Arrays.deepToString(revealed));
 		game.print();
-		System.out.println("Opening reveal (1, 1):");
-		System.out.println("Revealed: " + Arrays.deepToString(game.reveal(1, 1)));
+		return revealed;
+	}
+
+	private void printField(String label) {
+		System.out.println(label);
 		game.print();
 	}
 
--- src/nbradham/minesweeperBot/Game.java
+++ src/nbradham/minesweeperBot/Game.java
@@ -13,9 +13,7 @@ import java.util.Random;
  */
 final class Game {
 
-	private static final byte F_REVEALED = 0x10, F_FLAGGED = 0x20, F_MINE = 0x40, M_COUNT = 0xF;
-
-	private final byte[][] field;
+	private final Tile[][] field;
 	private final byte mines;
 	private boolean first = true;
 
@@ -27,7 +25,10 @@ final class Game {
 	 * @param mineCount The number of mines.
 	 */
 	public Game(byte cols, byte rows, byte mineCount) {
-		field = new byte[cols][rows];
+		field = new Tile[cols][rows];
+		for (byte x = 0; x < field.length; ++x)
+			for (byte y = 0; y < field[x].length; ++y)
+				field[x][y] = new Tile();
 		mines = mineCount;
 	}
 
@@ -37,34 +38,36 @@ final class Game {
 	void print() {
 		for (int y = field[0].length - 1; y >= 0; --y) {
 			for (byte x = 0; x < field.length; ++x)
-				System.out.printf("%s|",
-						flag(field[x][y], F_REVEALED) ? flag(field[x][y], F_MINE) ? "X" : getCount(x, y)
-								: flag(field[x][y], F_FLAGGED) ? "P" : "-");
+				System.out.printf("%s ",
+						isRevealed(x, y) ? field[x][y].mine ? "X" : field[x][y].count == 0 ? " " : field[x][y].count
+								: field[x][y].flag ? "+" : ".");
 			System.out.println();
 		}
+		System.out.println('=');
 	}
 
 	/**
 	 * Reveals ({@code x}, {@code y}). If that tile does not have any mines around
 	 * it, the adjacent tiles will be revealed as well and this will repeat until
-	 * mines are present.
+	 * mines are present in adjacent tiles.
 	 * 
 	 * @param x The x coordinate.
 	 * @param y The y coordinate.
-	 * @return A 2D integer array containing the coordinates of all revealed tiles.
+	 * @return A 2D integer array containing the coordinates of all revealed tiles
+	 *         with adjacent mines.
 	 */
 	int[][] reveal(int x, int y) {
 		if (first) {
 			ArrayList<byte[]> cords = new ArrayList<>();
 			for (byte c = 0; c < field.length; ++c)
-				for (byte r = 0; r < field[x].length; ++r)
-					if (c != x && r != y)
+				for (byte r = 0; r < field[c].length; ++r)
+					if (isFartherThanOne(c, x) && isFartherThanOne(r, y))
 						cords.add(new byte[] { c, r });
 			Random r = new Random();
 			byte[] rem;
 			for (byte n = 0; n < mines; ++n) {
-				field[(rem = cords.remove(r.nextInt(cords.size())))[0]][rem[1]] |= F_MINE;
-				forNeighbors(rem[0], rem[1], (incX, incY) -> field[incX][incY] += 1);
+				field[(rem = cords.remove(r.nextInt(cords.size())))[0]][rem[1]].mine = true;
+				forNeighbors(rem[0], rem[1], (incX, incY) -> field[incX][incY].count += 1);
 			}
 			first = false;
 		}
@@ -74,14 +77,19 @@ final class Game {
 		queue.add(new int[] { x, y });
 		int[] cords;
 		while (queue.size() > 0) {
-			revealed.add(cords = queue.poll());
-			field[cords[0]][cords[1]] |= F_REVEALED;
-			if (getCount(cords[0], cords[1]) == 0)
+			field[(cords = queue.poll())[0]][cords[1]].revealed = true;
+			revealed.add(cords);
+			if (field[cords[0]][cords[1]].count == 0)
 				forNeighbors(cords[0], cords[1], (nX, nY) -> {
-					if ((field[nX][nY] & F_REVEALED) != F_REVEALED)
-						queue.offer(new int[] { nX, nY });
+					int[] tmp;
+					if (isHidden(nX, nY) && isUnique(queue, tmp = new int[] { nX, nY }))
+						queue.offer(tmp);
 				});
 		}
+
+		if (isGameOver())
+			System.out.println("Game Over.");
+
 		return revealed.toArray(new int[0][]);
 	}
 
@@ -93,7 +101,7 @@ final class Game {
 	 * @param y       The y coordinate.
 	 * @param handler The NeighborHandler to call.
 	 */
-	private void forNeighbors(int x, int y, NeighborHandler handler) {
+	void forNeighbors(int x, int y, NeighborHandler handler) {
 		int maxX = Math.min(x + 2, field.length), maxY;
 		for (int incX = Math.max(x - 1, 0); incX < maxX; ++incX) {
 			maxY = Math.min(y + 2, field[incX].length);
@@ -103,25 +111,60 @@ final class Game {
 		}
 	}
 
-	/**
-	 * Retrieves the reported mine count of ({@code x}, {@code y});
-	 * 
-	 * @param x The x coordinate.
-	 * @param y The y coordinate.
-	 * @return The number of adjacent mines.
-	 */
-	private int getCount(int x, int y) {
-		return field[x][y] & M_COUNT;
+	int getMineCount(int x, int y) {
+		if (isRevealed(x, y))
+			return field[x][y].count;
+		return -1;
 	}
 
-	/**
-	 * Checks if {@code var} contains {@code flag} bit.
-	 * 
-	 * @param var  The variable to check.
-	 * @param flag The flag to check.
-	 * @return True if {@code flag} bit is true.
-	 */
-	private static boolean flag(byte var, byte flag) {
-		return (var & flag) == flag;
+	boolean isHidden(int x, int y) {
+		return !isRevealed(x, y);
+	}
+
+	boolean isRevealed(int x, int y) {
+		return field[x][y].revealed;
+	}
+
+	boolean isFlagged(int x, int y) {
+		return field[x][y].flag;
+	}
+
+	boolean isGameOver() {
+		for (Tile[] c : field)
+			for (Tile t : c)
+				if (t.revealed) {
+					if (t.mine)
+						return true;
+				} else if (!t.mine)
+					return false;
+		return true;
+	}
+
+	int getWidth() {
+		return field.length;
+	}
+
+	int getHeight() {
+		return field[0].length;
+	}
+
+	void flag(int x, int y) {
+		field[x][y].flag = isHidden(x, y);
+	}
+
+	private static boolean isUnique(Queue<int[]> cords, int[] test) {
+		for (int[] set : cords)
+			if (set[0] == test[0] && set[1] == test[1])
+				return false;
+		return true;
+	}
+
+	private static boolean isFartherThanOne(int a, int b) {
+		return Math.abs(a - b) > 1;
+	}
+
+	private static final class Tile {
+		byte count;
+		boolean mine, revealed, flag;
 	}
 }
